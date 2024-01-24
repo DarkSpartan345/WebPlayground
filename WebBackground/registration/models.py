2c38ff57cdfb88c6c0422103e910b1f54412ff6d
DarkSpartan345 <raruedam@udistrital.edu.co>
2024-01-24 18:02:00 -0500

    optimizacion y detalles app registration
    optimizacion de las imagenes de los perfiles,y pruebas unitarias para verificacion de creacion

--- WebBackground/registration/models.py
+++ WebBackground/registration/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+def custom_upload_profile(instance,filename):
+    old_instance=Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return "profile"+filename
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    avatar=models.ImageField(null=True,blank=True,upload_to="profiles")
+    avatar=models.ImageField(null=True,blank=True,upload_to=custom_upload_profile)
     bio= models.TextField(null=True,blank=True)
     link=models.URLField(null=True,blank=True,max_length=200)
+ 
+@receiver(post_save,sender=User)    
+def ensure_profile_exists(sender,instance,**kwargs):
+    if kwargs.get("created",False):
+        Profile.objects.get_or_create(user=instance)
+        print("se ha creado existosamente el usuario y perfil enlazado")
     
-# Create your models here.
