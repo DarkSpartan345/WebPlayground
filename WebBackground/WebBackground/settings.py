52de011c66fd2e168564aace43fb948444d5a196
DarkSpartan345 <raruedam@udistrital.edu.co>
2024-01-24 14:33:51 -0500

    app registration Completada
    se generaron todos los templates para que el sistema tuviera formularios de crear cuenta, editar perfil, etc

--- WebBackground/WebBackground/settings.py
+++ WebBackground/WebBackground/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #auth config
-LOGIN_REDIRECT_URL='pages:pages'
+# LOGIN_REDIRECT_URL='pages:pages'
 LOGOUT_REDIRECT_URL='home'
 
 #Email
--- WebBackground/registration/forms.py
+++ WebBackground/registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserCreationFormWithEmail(UserCreationForm):
     email=forms.EmailField(required=True,help_text="Obligatorio, debe contener maximo 254 carateres y debe ser valido")
@@ -14,4 +15,28 @@ class UserCreationFormWithEmail(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Este Correo ya ha sido registrado")
         return email
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=["avatar","bio","link"]
+        widgets={
+            "avatar":forms.ClearableFileInput(attrs={'class':"form-control-file mt-3"}),
+            "bio":forms.Textarea(attrs={"class":"form-control","row":3,"placeholder":"Biografia"}),
+            "link":forms.URLInput(attrs={"class":"form-control","placeholder":"Enlace"})
+            
+        }
+class EmailForm(forms.ModelForm):
+    email=forms.EmailField(required=True,help_text="Obligatorio, debe contener maximo 254 carateres y debe ser valido")
+    class Meta:
+         model=User
+         fields=["email"]
+         
+    def clean_email(self):
+        email=self.cleaned_data.get("email")
+        if "email" in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("Este Correo ya ha sido registrado")
+            return email
+        
 
